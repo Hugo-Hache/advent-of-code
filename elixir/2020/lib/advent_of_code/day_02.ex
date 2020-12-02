524d7ffad254563dfcdc149761feda69f86857e7
Hugo Hache <hugo.hache@fabernovel.com>
2020-12-02 10:58:37 +0100

    Refacto 2020 day 2

--- elixir/2020/lib/advent_of_code/day_02.ex
+++ elixir/2020/lib/advent_of_code/day_02.ex
@@ -1,15 +1,7 @@
 defmodule AdventOfCode.Day02 do
   def part1(input) do
     input
-    |> String.split("\n")
-    |> Enum.filter(&(String.length(&1) > 0))
-    |> Enum.map(&String.split(&1, " "))
-    |> Enum.map(fn [occurence_range, <<letter::binary-size(1), ":">>, password] ->
-      [min_occurence, max_occurence] =
-        occurence_range |> String.split("-") |> Enum.map(&String.to_integer/1)
-
-      {min_occurence, max_occurence, letter, password}
-    end)
+    |> parse_policies()
     |> Enum.filter(fn {min_occurence, max_occurence, letter, password} ->
       occurence =
         password
@@ -23,6 +15,27 @@ defmodule AdventOfCode.Day02 do
 
   def part2(input) do
     input
+    |> parse_policies()
+    |> Enum.filter(fn {lower_index, higher_index, letter, password} ->
+      occurence_at_indices =
+        password
+        |> String.graphemes()
+        |> Enum.with_index(1)
+        |> Enum.reduce(0, fn {l, i}, occurence_at_indices ->
+          if l == letter && (i == lower_index || i == higher_index) do
+            occurence_at_indices + 1
+          else
+            occurence_at_indices
+          end
+        end)
+
+      occurence_at_indices == 1
+    end)
+    |> length()
+  end
+
+  def parse_policies(input) do
+    input
     |> String.split("\n")
     |> Enum.filter(&(String.length(&1) > 0))
     |> Enum.map(&String.split(&1, " "))
@@ -32,21 +45,5 @@ defmodule AdventOfCode.Day02 do
 
       {min_occurence, max_occurence, letter, password}
     end)
-    |> Enum.filter(fn {lower_index, higher_index, letter, password} ->
-      count =
-        password
-        |> String.graphemes()
-        |> Enum.with_index(1)
-        |> Enum.reduce(0, fn {l, i}, count ->
-          if l == letter && (i == lower_index || i == higher_index) do
-            count + 1
-          else
-            count
-          end
-        end)
-
-      count == 1
-    end)
-    |> length()
   end
 end
